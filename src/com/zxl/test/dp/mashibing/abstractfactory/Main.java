74326c6a2d0ca3cb03723cd863f4e92fcac02db2
zhangxiaoliang-code <zxll2007@126.com>
2019-11-22 11:18:49 +0800

    Revert "测试git修改功能"
    This reverts commit 5d7c525e199fc8a0020d01f3ca2ce906866d033e.

--- src/com/zxl/test/dp/mashibing/abstractfactory/Main.java
+++ src/com/zxl/test/dp/mashibing/abstractfactory/Main.java
@@ -3,7 +3,7 @@ package com.zxl.test.dp.mashibing.abstractfactory;
 public class Main {
 
 	public static void main(String[] args) {
-		AbstractFactory f = new ModernFactory();
+		AbstractFactory f = new MagicFactory();
 		
 		Vehicle c = f.createVehicle();
 		c.go();
