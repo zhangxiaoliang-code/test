cf35c1e365e568ba60219dd715624634dc0072d7
zhangxiaoliang-code <zxll2007@126.com>
2019-12-10 11:10:49 +0800

    Student换位置

--- src/com/zxl/test/Test.java
+++ src/com/zxl/test/Test.java
@@ -87,3 +87,55 @@ public class Test {
 	}
 
 }
+
+class Student {
+
+	private int id;
+	
+	private String name;
+	
+	private Integer age;
+	
+	private String other;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getOther() {
+		return other;
+	}
+
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	public Student(int id, String name, Integer age, String other) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.other = other;
+	}
+	
+}
